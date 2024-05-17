85653e2b2819fd6cb02a25e42264300e457d2348
mitso-ksr <mitsonary@gmail.com>
2024-05-17 16:47:25 +0330

    the add notes extra reducers and Thunk middleware

--- src/store/notesSlice.js
+++ src/store/notesSlice.js
@@ -1,45 +1,85 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { addDoc, collection, query , where, getDocs, deleteDoc, doc} from 'firebase/firestore';
+import { db } from '../firebase/config';  
 
 export const notesSlice = createSlice({
   name: 'notes',
-  initialState: [
-    {
-      id: 1,
-      book_id: 1,
-      title:"Page 18 - On Europe's Decline",
-      text: "The leading states of the European Union, and in particular of the eurozone, were dogged by a growing sense of decline. Their production systems and their societies that were said to be in decline, rather than Europe as a whole."
-    },
-    {
-      id: 2,
-      book_id: 1,
-      title:"Page 55 - Treaty on Friendship and Cooperation",
-      text: "The Portuguese and Spanish Governments signed the Treaty on Friendship and Cooperation at the 32nd Luso-Spanish Summit held in Trujillo in October 2021. This followed on from the commitment undertaken at the Guarda Summit in October 2020."
-    },
-    {
-      id: 3,
-      book_id: 2,
-      title:"Page 61 - On Mesopotamia",
-      text: "Jane R. McIntosh wrote the first general introduction to Mesopotamia that covers all four of the area's major ancient civilizations―Sumer, Akkad, Assyria, and Babylonia."
-    }
-    
-    ],
+  initialState:{
+    notes:[],
+    status: 'idle'
+  },
   reducers: {
-    addNote: (notes, action) => {
-      let newNote = action.payload;
-      newNote.id = notes.length ? Math.max(...notes.map(note=> note.id)) + 1 : 1;
-      notes.push(newNote);
-    },
-    eraseNote: (notes, action) => {
-        return notes.filter(note => note.id != action.payload);
-    },
-    eraseBookNotes: (notes, action) => {
-        return notes.filter(note => note.book_id != action.payload);
-    }
+    // addNote: (notes, action) => {
+    //   let newNote = action.payload;
+    //   newNote.id = notes.length ? Math.max(...notes.map(note=> note.id)) + 1 : 1;
+    //   notes.push(newNote);
+    // },
+    // eraseNote: (notes, action) => {
+    //     return notes.filter(note => note.id != action.payload);
+    // },
+    // eraseBookNotes: (notes, action) => {
+    //     return notes.filter(note => note.book_id != action.payload);
+    // }
+  },
+  extraReducers(builder){
+    builder
+    .addCase(fetchNotes.fulfilled, (state, action) => {
+      state.status = "succeeded"
+      state.notes = action.payload
+    })
+    .addCase(addNote.fulfilled , (state, action) => {
+      state.status = 'succeeded'
+      state.notes.push(action.payload)
+    })
+    .addCase(eraseNote.fulfilled, (state, action) => {
+      state.status = 'succeeded'
+      state.notes = state.notes.filter(note => note.id != action.payload)
+    })
+    .addCase(eraseBookNotes.fulfilled, (state, action) => {
+      state.status = "succeeded";
+      state.notes = state.notes.filter(note => note.book_id != action.payload)
+    })
+
   }
 })
 
-export const { addNote, eraseNote, eraseBookNotes } = notesSlice.actions;
+export const {  } = notesSlice.actions;
 
 export const selectNotes = state => state.notes;
 
 export default notesSlice.reducer;
+
+
+export const fetchNotes = createAsyncThunk("notes/fetchNotes", async(payload) => {
+  const q = query(collection(db, 'notes'), where ('book_id', '==', payload))
+  const querySnapshot = await getDocs(q);
+  const notesList = [];
+  querySnapshot.forEach((doc) => {
+    notesList.push({
+      id: doc.id,
+      ...doc.data()
+    })
+  })
+  return notesList
+})
+
+export const addNote = createAsyncThunk("notes/addNote" , async(payload) => {
+  const collectionRef = collection(db, "notes");
+  let newNote = payload;
+  const docRef = await addDoc(collectionRef, newNote)
+  newNote.id = docRef.id;
+  return newNote
+
+})
+
+export const eraseNote = createAsyncThunk("notes/eraseNote" , async(payload) => {
+  await deleteDoc(doc(db, 'notes', payload))
+  return payload
+})
+
+
+export const eraseBookNotes = createAsyncThunk("notes/eraseBookNotes", async(payload) => {
+  console.log("erasebooknotes fired")
+  const collectionRef = collection(db, 'notes')
+  
+})
